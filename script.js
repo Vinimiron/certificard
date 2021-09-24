650928388d2f4b7b6fd95c76dd1c0b67f7f7b36a
Vinimiron <vini_miron@hotmail.com>
2021-09-24 11:34:43 -0300

    Adicionado projeto inicial certificard

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//Desafios Aula 9:
+//1 -Terminar todo o design que está no figma (disponível abaixo)
+//2- Adicionar suas redes sociais de forma que quem clique consiga abrí-las
